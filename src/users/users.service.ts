7e2e686a31b4901c8b05f519447affeed3cc4617
Arthur Lopes <arthurlopr12@gmail.com>
2023-08-13 20:39:52 -0300

    feat: add categories

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -11,18 +11,47 @@ export class UsersService {
 
     const emailTaken = await this.prismaService.user.findUnique({
       where: { email },
+      select: { id: true },
     });
 
     if (emailTaken) {
+      console.log(emailTaken);
       throw new ConflictException('This email is already in use');
     }
 
     const hashedPassword = await hash(password, 12);
 
     const user = await this.prismaService.user.create({
-      data: { name, email, password: hashedPassword },
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        categories: {
+          createMany: {
+            data: [
+              // Income
+              { name: 'Salário', icon: 'salary', type: 'INCOME' },
+              { name: 'Freelance', icon: 'freelance', type: 'INCOME' },
+              { name: 'Outro', icon: 'other', type: 'INCOME' },
+              // Expense
+              { name: 'Casa', icon: 'home', type: 'EXPENSE' },
+              { name: 'Alimentação', icon: 'food', type: 'EXPENSE' },
+              { name: 'Educação', icon: 'education', type: 'EXPENSE' },
+              { name: 'Lazer', icon: 'fun', type: 'EXPENSE' },
+              { name: 'Mercado', icon: 'grocery', type: 'EXPENSE' },
+              { name: 'Roupas', icon: 'clothes', type: 'EXPENSE' },
+              { name: 'Transporte', icon: 'transport', type: 'EXPENSE' },
+              { name: 'Viagem', icon: 'travel', type: 'EXPENSE' },
+              { name: 'Outro', icon: 'other', type: 'INCOME' },
+            ],
+          },
+        },
+      },
     });
 
-    return user;
+    return {
+      name: user.name,
+      email: user.email,
+    };
   }
 }
