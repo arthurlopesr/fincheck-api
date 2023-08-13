b0f04b39b11ef03c0bdf0cfc913498e6e96b2f9c
Arthur Lopes <arthurlopr12@gmail.com>
2023-08-13 19:21:52 -0300

    feat: add validation email is in use

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return createUserDto;
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const { name, email, password } = createUserDto;
+
+    const emailTaken = await this.prismaService.user.findUnique({
+      where: { email },
+    });
+
+    if (emailTaken) {
+      throw new ConflictException('This email is already in use');
+    }
+
+    const user = await this.prismaService.user.create({
+      data: { name, email, password },
+    });
+
+    return user;
   }
 }
