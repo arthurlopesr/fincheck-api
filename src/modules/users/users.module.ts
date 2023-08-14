c4897feac0bf4ff921521583b870f0020fcb817e
Arthur Lopes <arthurlopr12@gmail.com>
2023-08-13 21:20:14 -0300

    feat: add data base module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { UsersRepository } from 'src/shared/database/repositories/user.respositories';
-import { PrismaService } from 'src/shared/database/prisma.service';
-
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, UsersRepository, PrismaService],
+  providers: [UsersService],
 })
 export class UsersModule {}
