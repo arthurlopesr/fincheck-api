ceddcfb0537270e83d0d13ce6ec79343fce366b5
Arthur Lopes <arthurlopr12@gmail.com>
2023-08-14 21:22:33 -0300

    feat: add environment variables validation

--- src/shared/config/env.ts
+++ src/shared/config/env.ts
@@ -1,3 +1,19 @@
-export const env = {
-  jwtSecret: process.env.JWT_SECRET,
-};
+import { plainToInstance } from 'class-transformer';
+import { IsNotEmpty, IsString, NotEquals, validateSync } from 'class-validator';
+
+class Env {
+  @IsString()
+  @IsNotEmpty()
+  @NotEquals('unsecure_jwt_secret')
+  jwtSecret: string;
+}
+
+export const env: Env = plainToInstance(Env, {
+  jwtScret: process.env.JWT_SECRET,
+});
+
+const errors = validateSync(Env);
+
+if (errors.length > 0) {
+  throw new Error(JSON.stringify(errors, null, 2));
+}
