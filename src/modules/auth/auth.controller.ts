01c1df74a103da8c2edf31b497fbba0a98356033
Arthur Lopes <arthurlopr12@gmail.com>
2023-08-16 19:32:43 -0300

    feat: auth service refactoring

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthenticateDto } from './dto/authenticate.dto';
-
+import { SignupDto } from './dto/create-user.dto';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -9,5 +9,10 @@ export class AuthController {
   @Post('signin')
   authenticate(@Body() authenticateDto: AuthenticateDto) {
     return this.authService.authenticate(authenticateDto);
+  }
+
+  @Post('signup')
+  create(@Body() signupDto: SignupDto) {
+    return this.authService.signup(signupDto);
   }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,8 +1,13 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthenticateDto } from './dto/authenticate.dto';
 import { UsersRepository } from 'src/shared/database/repositories/user.respositories';
-import { compare } from 'bcryptjs';
+import { compare, hash } from 'bcryptjs';
 import { JwtService } from '@nestjs/jwt';
+import { SignupDto } from './dto/create-user.dto';
 
 @Injectable()
 export class AuthService {
@@ -31,5 +36,54 @@ export class AuthService {
     const accessToken = await this.jwtService.signAsync({ sub: user.id });
 
     return { accessToken };
+  }
+
+  async signup(signupDto: SignupDto) {
+    const { name, email, password } = signupDto;
+
+    const emailTaken = await this.usersRepo.findUnique({
+      where: { email },
+      select: { id: true },
+    });
+
+    if (emailTaken) {
+      console.log(emailTaken);
+      throw new ConflictException('This email is already in use');
+    }
+
+    const hashedPassword = await hash(password, 12);
+
+    const user = await this.usersRepo.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        categories: {
+          createMany: {
+            data: [
+              // Income
+              { name: 'Salário', icon: 'salary', type: 'INCOME' },
+              { name: 'Freelance', icon: 'freelance', type: 'INCOME' },
+              { name: 'Outro', icon: 'other', type: 'INCOME' },
+              // Expense
+              { name: 'Casa', icon: 'home', type: 'EXPENSE' },
+              { name: 'Alimentação', icon: 'food', type: 'EXPENSE' },
+              { name: 'Educação', icon: 'education', type: 'EXPENSE' },
+              { name: 'Lazer', icon: 'fun', type: 'EXPENSE' },
+              { name: 'Mercado', icon: 'grocery', type: 'EXPENSE' },
+              { name: 'Roupas', icon: 'clothes', type: 'EXPENSE' },
+              { name: 'Transporte', icon: 'transport', type: 'EXPENSE' },
+              { name: 'Viagem', icon: 'travel', type: 'EXPENSE' },
+              { name: 'Outro', icon: 'other', type: 'INCOME' },
+            ],
+          },
+        },
+      },
+    });
+
+    return {
+      name: user.name,
+      email: user.email,
+    };
   }
 }
