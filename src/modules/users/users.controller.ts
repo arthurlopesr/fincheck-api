75c4f747741b0bc98b168954ae0f0cf72214d274
Arthur Lopes <arthurlopr12@gmail.com>
2023-08-16 22:07:49 -0300

    feat: add auth guard

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get('/me')
+  me(@Req() request: any) {
+    console.log({ meUserId: request.userId });
+
+    return this.usersService.getUserById('userId');
+  }
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  getUserById(userId: string) {
+    return { userId };
+  }
+}
