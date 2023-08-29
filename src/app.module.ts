ddfb0c6c551e580c7adafc4fffa4378c9f28227c
Arthur Lopes <arthurlopr12@gmail.com>
2023-08-29 19:48:53 -0300

    feat: add dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './modules/users/users.module';
-import { DatabaseModule } from './shared/database/database.module';
-import { AuthModule } from './modules/auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
+
+import { DatabaseModule } from './shared/database/database.module';
+import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
 import { AuthGuard } from './modules/auth/auth.guard';
 
 @Module({
