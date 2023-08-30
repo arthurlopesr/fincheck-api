2cf38c4a3da5cf84f68e6a08bbafa5ea76272ed0
Arthur Lopes <arthurlopr12@gmail.com>
2023-08-29 21:11:41 -0300

    feat: crate custom decorator IsPublic

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,6 +2,9 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SinginDto } from './dto/singin.dto';
 import { SignupDto } from './dto/singup.dto';
+import { IsPublic } from 'src/shared/decorators/IsPublic';
+
+@IsPublic()
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
