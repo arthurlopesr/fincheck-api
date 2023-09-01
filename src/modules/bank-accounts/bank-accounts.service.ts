902a94a8aea74f8de6e0d50b4286186ff0359683
Arthur Lopes <arthurlopr12@gmail.com>
2023-08-31 21:14:57 -0300

    feat: create bank account method

--- src/modules/bank-accounts/bank-accounts.service.ts
+++ src/modules/bank-accounts/bank-accounts.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBankAccountDto } from './dto/create-bank-account.dto';
 import { UpdateBankAccountDto } from './dto/update-bank-account.dto';
+import { BankAccountsRepository } from 'src/shared/database/repositories/bank-accounts.respositories';
 
 @Injectable()
 export class BankAccountsService {
+  constructor(private readonly bankAccountRepo: BankAccountsRepository) {}
+
   create(userId: string, createBankAccountDto: CreateBankAccountDto) {
-    return { userId, createBankAccountDto };
+    const { color, initialBalance, name, type } = createBankAccountDto;
+
+    return this.bankAccountRepo.create({
+      data: {
+        userId,
+        color,
+        initialBalance,
+        name,
+        type,
+      },
+    });
   }
 
   findAll() {
