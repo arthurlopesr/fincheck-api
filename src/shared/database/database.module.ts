28b58324f4598f792040b68dfed8594a0c134742
Arthur Lopes <arthurlopr12@gmail.com>
2023-08-31 21:07:53 -0300

    feat: add bank accout respository

--- src/shared/database/database.module.ts
+++ src/shared/database/database.module.ts
@@ -2,10 +2,16 @@ import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { UsersRepository } from './repositories/user.respositories';
 import { CategoriesRepository } from './repositories/categories.respositories';
+import { BankAccountsRepository } from './repositories/bank-accounts.respositories';
 
 @Global()
 @Module({
-  providers: [PrismaService, UsersRepository, CategoriesRepository],
-  exports: [UsersRepository, CategoriesRepository],
+  providers: [
+    PrismaService,
+    UsersRepository,
+    CategoriesRepository,
+    BankAccountsRepository,
+  ],
+  exports: [UsersRepository, CategoriesRepository, BankAccountsRepository],
 })
 export class DatabaseModule {}
