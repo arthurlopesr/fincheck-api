98d688621bae91283f19a1e113d0052b154452dc
Arthur Lopes <arthurlopr12@gmail.com>
2023-09-02 10:27:22 -0300

    feat: add dto validation and transactions entities

--- src/modules/transactions/dto/create-transaction.dto.ts
+++ src/modules/transactions/dto/create-transaction.dto.ts
@@ -1,1 +1,38 @@
-export class CreateTransactionDto {}
+import {
+  IsDateString,
+  IsNotEmpty,
+  IsPositive,
+  IsString,
+  IsUUID,
+  IsEnum,
+} from 'class-validator';
+import { TransactionType } from '../entities/Transaction';
+
+export class CreateTransactionDto {
+  @IsString()
+  @IsNotEmpty()
+  @IsUUID()
+  bankAccountId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsUUID()
+  categoryId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsPositive()
+  value: number;
+
+  @IsNotEmpty()
+  @IsDateString()
+  date: string;
+
+  @IsNotEmpty()
+  @IsEnum(TransactionType)
+  type: TransactionType;
+}
